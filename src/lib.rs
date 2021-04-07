a5a664e848d22c37db6745dc89813b3f38cde6d5
jayphbee <jayphbee@163.com>
2021-04-08 03:02:53 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
+use std::collections::BTreeSet;
 use std::fmt;
+use std::num;
+use std::str::FromStr;
 
-use nom::{Err, error::{Error, ErrorKind}, IResult};
-use nom::branch::alt;
-use nom::branch::permutation;
-use nom::bytes::complete::tag;
-
-use chrono::Local;
-
+use chrono::{DateTime, Datelike, Duration, TimeZone, Timelike, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TimerConfig {
+pub struct TimerConfig {
     sec: String,
     min: String,
     hour: String,
@@ -23,85 +20,148 @@ struct TimerConfig {
     year: String,
 }
 
+impl TimerConfig {
+    pub fn parse<TZ: TimeZone>(&self, dt: &DateTime<TZ>) -> Result<DateTime<TZ>, ParseError> {
+        let tz = dt.timezone();
+        let mut next = Utc.from_local_datetime(&dt.naive_local()).unwrap() + Duration::minutes(1);
+
+        next =
+            Utc.ymd(next.year(), next.month(), next.day())
+                .and_hms(next.hour(), next.minute(), 0);
+
+        let result = loop {
+            // 最多10年
+            if next.year() - dt.year() > 10 {
+                return Err(ParseError::InvalidValue);
+            }
+
+            // month
+            let month = parse_field(&self.month, 1, 12)?;
+            // dbg!(&month);
+            if !month.contains(&next.month()) {
+                if next.month() == 12 {
+                    next = Utc.ymd(next.year() + 1, 1, 1).and_hms(0, 0, 0);
+                } else {
+                    next = Utc.ymd(next.year(), next.month() + 1, 1).and_hms(0, 0, 0);
+                }
+                continue;
+            }
+
+            // day of month
+            let do_m = parse_field(&self.day_of_month, 1, 31)?;
+            // dbg!(&do_m);
+            if !do_m.contains(&next.day()) {
+                next = next + Duration::days(1);
+                next = Utc
+                    .ymd(next.year(), next.month(), next.day())
+                    .and_hms(0, 0, 0);
+                continue;
+            }
+
+            // hour
+            let hour = parse_field(&self.hour, 0, 23)?;
+            // dbg!(&hour);
+            if !hour.contains(&next.hour()) {
+                next = next + Duration::hours(1);
+                next = Utc
+                    .ymd(next.year(), next.month(), next.day())
+                    .and_hms(next.hour(), 0, 0);
+                continue;
+            }
+
+            // minute
+            let minute = parse_field(&self.min, 0, 59)?;
+            // dbg!(&minute);
+            if !minute.contains(&next.minute()) {
+                next = next + Duration::minutes(1);
+                next = Utc.ymd(next.year(), next.month(), next.day()).and_hms(
+                    next.hour(),
+                    next.minute(),
+                    next.second(),
+                );
+                println!(
+                    "next minite {:?}, next sec {:?}",
+                    next.minute(),
+                    next.second()
+                );
+                continue;
+            }
+
+            // second
+            let seconds = parse_field(&self.sec, 0, 59)?;
+            // dbg!(&seconds);
+            if !seconds.contains(&next.second()) {
+                next = next + Duration::seconds(1);
+                continue;
+            }
+
+            // day of week
+            let do_w = parse_field(&self.day_of_week, 0, 6)?;
+            // dbg!(&do_w);
+            if !do_w.contains(&next.weekday().num_days_from_sunday()) {
+                next = next + Duration::days(1);
+                continue;
+            }
+
+            if let Some(dt) = tz.from_local_datetime(&next.naive_local()).latest() {
+                break dt;
+            }
+
+            next = next + Duration::minutes(1);
+        };
+
+        Ok(result)
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum ParseError {
-    SecOutOfBound,
-    MinOutOfBound,
-    HourOutOfBound,
-    MonthOutOfBound,
-    WeekOfMonthOutOfBound,
-    DayOfMonthOutOfBound,
-    DayOfYearOutOfBound,
-    DayOfWeekOutOfBound,
-    WeekOfYearOutOfBound,
-
-    ConfigSyntaxError,
+    InvalidValue,
+    InvalidRange,
+    ParseIntError(num::ParseIntError),
+    TryFromIntError(num::TryFromIntError),
 }
 
-#[derive(Debug)]
-struct TimeRecord {
-    sec: [u8; 60],
-    min: [u8; 60],
-    hour: [u8; 24],
-    day_of_month: [u8; 31],
-    month: [u8; 13],
-    week_of_month: [u8; 4],
-    day_of_week: [u8; 7],
-    day_of_year: [u8; 366 + 31],
-    week_of_year: [u8; 57],
-    year: u32,
+enum Dow {
+    Sun = 0,
+    Mon = 1,
+    Tue = 2,
+    Wed = 3,
+    Thu = 4,
+    Fri = 5,
+    Sat = 6,
 }
 
-impl Default for TimeRecord {
-    fn default() -> Self {
-        Self {
-            sec: [0; 60],
-            min: [0; 60],
-            hour: [0; 24],
-            day_of_month: [0; 31],
-            month: [0; 13],
-            week_of_month: [0; 4],
-            day_of_week: [0; 7],
-            day_of_year: [0; 397],
-            week_of_year: [0; 57],
-            // TODO
-            year: 0
+impl FromStr for Dow {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &*s.to_uppercase() {
+            "SUN" => Ok(Self::Sun),
+            "MON" => Ok(Self::Mon),
+            "TUE" => Ok(Self::Tue),
+            "WED" => Ok(Self::Wed),
+            "THU" => Ok(Self::Thu),
+            "FRI" => Ok(Self::Fri),
+            "SAT" => Ok(Self::Sat),
+            _ => Err(()),
         }
     }
 }
 
-struct TimeRecordBuilder {
-    
+impl std::error::Error for ParseError {}
+
+impl From<num::ParseIntError> for ParseError {
+    #[must_use]
+    fn from(err: num::ParseIntError) -> Self {
+        Self::ParseIntError(err)
+    }
 }
 
-impl TimeRecord {
-    /// 通过解析配置构造TimeRecord
-    fn from_config(config: TimerConfig) -> TimeRecord {
-        todo!()
-    }
-
-    fn from_fake_config() -> TimeRecord {
-        /// fake
-        Self {
-            sec: [0; 60],
-            min: [1; 60],
-            hour: [1; 24],
-            day_of_month: [1; 31],
-            month: [1; 13],
-            week_of_month: [1; 4],
-            day_of_year: [1; 397],
-            day_of_week: [1; 7],
-            week_of_year: [1; 57],
-            year: 2021
-        }
-    }
-
-    /// 未来触发的时间点
-    fn upcomming(&mut self) {
-    }
-
-    /// 下一次触发时间点
-    fn next(&mut self) {
-
+impl From<num::TryFromIntError> for ParseError {
+    #[must_use]
+    fn from(err: num::TryFromIntError) -> Self {
+        Self::TryFromIntError(err)
     }
 }
 
@@ -109,62 +169,136 @@ impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use ParseError::*;
         match self {
-            SecOutOfBound => write!(f, "sec out of bound"),
-            MinOutOfBound => write!(f, "min out of bound"),
-            HourOutOfBound => write!(f, "hour out of bound"),
-            MonthOutOfBound => write!(f, "month out of bound"),
-            WeekOfMonthOutOfBound => write!(f, "week of month out of bound"),
-            DayOfMonthOutOfBound => write!(f, "day of month out of bound"),
-            DayOfYearOutOfBound => write!(f, "day of year out of bound"),
-            DayOfWeekOutOfBound => write!(f, "day of week out of bound"),
-            WeekOfYearOutOfBound => write!(f, "week of year out of bound"),
-            ConfigSyntaxError => write!(f, "config syntax error"),
+            InvalidRange => write!(f, "invalid range"),
+            InvalidValue => write!(f, "invalid value"),
+            ParseIntError(err) => err.fmt(f),
+            TryFromIntError(err) => err.fmt(f),
         }
     }
 }
 
-fn parse_sec(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
+pub fn parse_field(field: &str, min: u32, max: u32) -> Result<BTreeSet<u32>, ParseError> {
+    let mut values = BTreeSet::<u32>::new();
+
+    let fields: Vec<&str> = field.split(',').filter(|s| !s.is_empty()).collect();
+
+    for field in fields {
+        match field {
+            // 匹配任意值
+            "*" => {
+                for i in min..=max {
+                    values.insert(i);
+                }
+            }
+            // 步进值
+            f if field.contains('/') => {
+                let tmp: Vec<&str> = f.split('/').collect();
+                if tmp.len() != 2 {
+                    return Err(ParseError::InvalidValue);
+                }
+
+                let start = tmp[0].parse::<u32>()?;
+                if start < min {
+                    return Err(ParseError::InvalidValue);
+                }
+                let step = tmp[1].parse::<u32>()?;
+
+                for i in (start..=max).step_by(step as usize) {
+                    values.insert(i);
+                }
+            }
+            //范围值
+            f if f.contains('-') => {
+                let tmp_fields: Vec<&str> = f.split('-').collect();
+                if tmp_fields.len() != 2 {
+                    return Err(ParseError::InvalidRange);
+                }
+
+                let mut fields: Vec<u32> = Vec::new();
+
+                if let Ok(dow) = Dow::from_str(tmp_fields[0]) {
+                    fields.push(dow as u32);
+                } else {
+                    fields.push(tmp_fields[0].parse::<u32>()?);
+                };
+
+                if let Ok(dow) = Dow::from_str(tmp_fields[1]) {
+                    fields.push(dow as u32);
+                } else {
+                    fields.push(tmp_fields[1].parse::<u32>()?);
+                }
+
+                if fields[0] > fields[1] || fields[1] > max {
+                    return Err(ParseError::InvalidRange);
+                }
+                for i in (fields[0]..=fields[1]).collect::<Vec<u32>>() {
+                    values.insert(i);
+                }
+            }
+            // TODO
+            _ => {
+                if let Ok(dow) = Dow::from_str(field) {
+                    values.insert(dow as u32);
+                } else {
+                    let f = field.parse::<u32>()?;
+                    if f > max {
+                        return Err(ParseError::InvalidValue);
+                    }
+                    values.insert(f);
+                }
+            }
+        }
+    }
+    Ok(values)
 }
 
-fn parse_min(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Local;
 
-fn parse_hour(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
+    #[test]
+    fn test2() {
+        let config = r#"
+            {
+                "sec": "1/2",
+                "min": "2",
+                "hour": "1/2",
+                "day_of_month": "*",
+                "month": "*",
+                "week_of_month": "*",
+                "day_of_week": "*",
+                "day_of_year": "*",
+                "week_of_year": "*",
+                "year": "*"
+            }
+        "#;
+        // let config = r#"
+        //     {
+        //         "sec": "1/2",
+        //         "min": "*",
+        //         "hour": "*",
+        //         "day_of_month": "*",
+        //         "month": "*",
+        //         "week_of_month": "*",
+        //         "day_of_week": "*",
+        //         "day_of_year": "*",
+        //         "week_of_year": "*",
+        //         "year": "*"
+        //     }
+        // "#;
 
-fn parse_month(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
+        let c: TimerConfig = serde_json::from_str(config).unwrap();
+        println!("config ==== {:?}", c);
+        let dt = Utc::now().with_timezone(&Local);
+        let res = c.parse(&dt).unwrap();
+        println!("res = {:?}", res);
+    }
 
-fn parse_week_of_month(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-fn parse_day_of_month(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-fn parse_day_of_year(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-fn parse_day_of_week(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-fn parse_week_of_year(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-fn parse_year(i: &str) -> Result<Vec<bool>, ParseError> {
-    todo!()
-}
-
-#[test]
-fn test_next_time() {
-    let local = Local::now();
-    dbg!(local);
+    #[test]
+    fn test_parse() {
+        let x = "1/2";
+        let res = parse_field(x, 1, 31).unwrap();
+        println!("res = {:?}", res);
+    }
 }
